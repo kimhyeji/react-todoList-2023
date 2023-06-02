bab4ca23bff00053971a31632d7fce18daaeec69
kimhyeji <gpwl5589@gamil.com>
2023-06-02 19:19:16 +0900

    할 일 작성 페이지 꾸미기

--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,10 +1,12 @@
+import { TextField } from "@mui/material";
+
 export default function WritePage() {
   return (
     <>
-      <div className="flex-1 flex justify-center items-center">
-        <div>
-          <span>글쓰기 페이지</span>
-        </div>
+      <div className="flex-1 flex p-10 flex-col gap-7">
+        <TextField label="언제 해야하요?" focused type="datetime-local" />
+
+        <TextField label="언제 해야하요?" className="flex-1 flex" multiline />
       </div>
     </>
   );
